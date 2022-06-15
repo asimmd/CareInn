543a2fc62b236f987ffeff236545ca3e119e0ee3
User <User@DESKTOP-5Q078GF>
2022-06-15 17:30:32 +0530

    Base updated

--- src/main/java/Generic_Utility/BaseClass.java
+++ src/main/java/Generic_Utility/BaseClass.java
@@ -22,7 +22,7 @@ import com.crm.comcast.objectrepository.HomePage;
 import com.crm.comcast.objectrepository.LoginPage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-// Base update
+// Base updated again - wb2
 public class BaseClass {
 
 	WebDriver_Utility wu = new WebDriver_Utility();
